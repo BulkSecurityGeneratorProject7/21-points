7e02e3c31289303cba5e295c9538ee6a06284915
Mithun <mithun@sw5.orangecounty.in>
2017-10-11 14:13:58 +0530

    created entity 'points'

--- src/main/java/com/evolveback/top/config/CacheConfiguration.java
+++ src/main/java/com/evolveback/top/config/CacheConfiguration.java
@@ -41,6 +41,7 @@ public class CacheConfiguration {
             cm.createCache(com.evolveback.top.domain.User.class.getName(), jcacheConfiguration);
             cm.createCache(com.evolveback.top.domain.Authority.class.getName(), jcacheConfiguration);
             cm.createCache(com.evolveback.top.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(com.evolveback.top.domain.Points.class.getName(), jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
---
+++ src/main/java/com/evolveback/top/repository/search/PointsSearchRepository.java
@@ -0,0 +1,10 @@
+package com.evolveback.top.repository.search;
+
+import com.evolveback.top.domain.Points;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the Points entity.
+ */
+public interface PointsSearchRepository extends ElasticsearchRepository<Points, Long> {
+}
